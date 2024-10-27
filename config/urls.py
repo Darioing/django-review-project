1f49247a7cb1db69334b706ba4bf9274f3e760c0
Dario <darioingannamorte@mail.ru>
2024-10-27 10:04:09 +0700

    Прописаны пути к приложениям в главном urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("review/", include("reviews.urls")),
+    path("user/", include("reviews.urls")),
     path('admin/', admin.site.urls),
 ]
