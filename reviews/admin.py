4e2733fe7387b1f5e932d87635634a9c85e93c4f
Dario <darioingannamorte@mail.ru>
2024-10-27 12:24:23 +0700

    Изменено отображение моделей в админке

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Places, PlacePhotos, Questions, Reviews, Comments, Votes
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'slug',
+    ]
+    list_filter = [
+        'name',
+    ]
+
+
+@admin.register(Places)
+class PlacesAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'address',
+        'category_id',
+        'slug',
+    ]
+    list_filter = [
+        'name',
+        'address',
+    ]
+
+@admin.register(PlacePhotos)
+class PlacePhotosAdmin(admin.ModelAdmin):
+    list_display = [
+        'place_id',
+        'image',
+    ]
+    list_filter = [
+        'place_id',
+    ]
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = [
+        'place_id',
+        'user_id',
+        'text',
+        'price',
+        'service',
+        'interior',
+        'created_at',
+    ]
+    list_filter = [
+        'place_id',
+        'text',
+    ]
+
+@admin.register(Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = [
+        'place_id',
+        'user_id',
+        'text',
+        'created_at',
+    ]
+    list_filter = [
+        'place_id',
+        'text',
+    ]
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_id',
+        'content_type',
+        'object_id',
+        'content_object',
+        'text',
+        'created_at',
+    ]
+    list_filter = [
+        'user_id',
+        'text',
+    ]
+
+@admin.register(Votes)
+class VotesAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_id',
+        'content_type',
+        'object_id',
+        'content_object',
+        'vote_type',
+        'created_at',
+    ]
+    list_filter = [
+        'user_id',
+    ]
