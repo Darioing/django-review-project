1f24df653c4e446bc57c03b579ed0f81c9c8c118
Dario <darioingannamorte@mail.ru>
2024-11-17 11:24:16 +0700

    Удаление ненужного импорта

--- reviews/models.py
+++ reviews/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from unidecode import unidecode
